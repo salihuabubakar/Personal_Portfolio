22d26f16b79baec2624329866a03d43fa0a0b3f6
salihu <57968062+salihuabubakar@users.noreply.github.com>
2022-03-03 23:07:04 +0100

    about page, contact page, and projects page update

---
+++ src/components/EmailForm.js
@@ -0,0 +1,35 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import {
+  formDiv
+} from "./layout.module.css";
+
+export const EmailForm = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("gmail", "my_email_id", e.target, "2_v9Zf9uTf5P8bmGG")
+      .then((result) => { console.log(result.text) },
+        (error) => { console.log(error.text) }
+      );
+      e.target.reset();
+  };
+
+  return (
+    <form className={formDiv} ref={form} onSubmit={sendEmail}>
+      <label>Subject</label>
+      <input type="text" required name="subject" />
+      <label>Name</label>
+      <input type="text" required name="name" />
+      <label>Email Address</label>
+      <input type="email" required name="email" />
+      <label>Your Message</label>
+      <textarea rows="6" required name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+
+export default EmailForm;
