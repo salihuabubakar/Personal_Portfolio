d2e0b4df1aef42497f322506f516ac2a3022f0de
salihu <57968062+salihuabubakar@users.noreply.github.com>
2022-09-03 12:46:32 +0100

    pwa config

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,10 +16,18 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
+        start_url: "/",
         icon: "src/images/favicon.png",
+        name: "Personal Portfolio",
+        short_name: "My Portfolio",
+        description: "This is my portfolio, it showcase my all technicall journey",
+        background_color: "#fff",
+        theme_color: "#ccc",
+        orientation: "portrait",
+        display: "standalone",
       },
     },
-
+    "gatsby-plugin-offline",
     "gatsby-plugin-mdx",
     "gatsby-transformer-sharp",
   ],
