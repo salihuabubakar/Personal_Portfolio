7835a0c11774d39a5d706658c8830c74e35a6727
salihu <57968062+salihuabubakar@users.noreply.github.com>
2022-09-03 14:01:07 +0100

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,38 +17,7 @@ module.exports = {
       resolve: "gatsby-plugin-manifest",
       options: {
         start_url: "/",
-        icons: [
-          {
-            src: "src/images/maskable192.png",
-            type: "image/png",
-            sizes: "192x192",
-            purpose: "any maskable",
-          },
-          {
-            src: "src/images/me192.png",
-            type: "image/png",
-            sizes: "192x192",
-          },
-          {
-            src: "src/images/me256.png",
-            type: "image/png",
-            sizes: "256x256",
-          },
-          {
-            src: "src/images/me384.png",
-            type: "image/png",
-            sizes: "384x384",
-          },
-          {
-            src: "src/images/me512.png",
-            type: "image/png",
-            sizes: "512x512",
-          },
-          {
-            src: "src/images/favicon.png",
-            type: "image/png",
-          },
-        ],
+        icon: "src/images/favicon.png",
         name: "Personal Portfolio",
         short_name: "My Portfolio",
         description:
